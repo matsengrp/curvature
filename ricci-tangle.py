2057fa739fe6cb5477fe9c8ad1d83d57f1f477ef
Erick Matsen <matsen@fhcrc.org>
2014-10-06 19:50:34 -0700

    Not sorting results in ricci-tangle.

--- ricci-tangle.py
+++ ricci-tangle.py
@@ -17,19 +17,18 @@ for n in (int(a) for a in sys.argv[1:]):
     adj_graph = Graph(matrix_of_csv(matrix_fname))
 
     def process_line(line):
-        row = list(int(x) for x in line)
-        t1_idx = row[0]
-        t2_idx = row[1]
+        row = list(line)
+        t1_idx = int(row[0])
+        t2_idx = int(row[1])
         if t1_idx == t2_idx:
             row.append("-")
         else:
-            row.append(ric_unif_rw(adj_graph, source=row[0], target=row[1]).ric)
+            row.append(ric_unif_rw(adj_graph, source=t1_idx, target=t2_idx).ric)
         return row
 
     with open(idx_fname, 'rb') as csvfile:
         idx_reader = csv.reader(csvfile, delimiter='\t', quotechar="'")
         results = list(process_line(line) for line in idx_reader)
-        results.sort(key=lambda r: r[3])
         with open("ricci"+str(n)+".mat", "w") as f_out:
             for r in results:
                 f_out.write("\t".join(str(e) for e in r)+"\n")
