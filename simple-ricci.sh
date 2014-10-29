8e5c4b754261280a294ff776ad43741dc0ac3c91
Erick Matsen <matsen@fhcrc.org>
2014-10-29 08:45:03 -0700

    Adding a simple ricci runner.

---
+++ simple-ricci.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+set -u
+
+test -z $1 && exit 1
+
+n_leaves=$1
+
+idx_path="tangles/tangle$n_leaves.idx"
+out_path="results/ricci$n_leaves.mat"
+
+./ricci-tangle.py --matrix matrices/matrix_$n_leaves --out $out_path $idx_path
