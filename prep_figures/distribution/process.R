3e14880dfaf58ebb9544b2843758b5800c1324e2
Erick Matsen <matsen@fhcrc.org>
2014-10-30 20:49:53 -0700

    Heat maps of curvature.

--- prep_figures/distribution/process.R
+++ prep_figures/distribution/process.R
@@ -1,3 +1,7 @@
+library(ape, apTreeshape)
+
+shape_of_str <- function(s) as.treeshape(read.tree(text=s))
+
 
 get_results <- function(path) {
     d <- read.table(path, stringsAsFactors=FALSE)
@@ -7,5 +11,16 @@ get_results <- function(path) {
     d$kappa <- sapply(d$kappa_str, function(s) eval(parse(text=s)))
     d$info= paste(d$t1_nwk, d$t2_nwk, paste("kappa:", d$kappa_str), sep="<br />")
     d$dist_jitter = d$dist + runif(nrow(d), min = -0.35, max = 0.35)
+
+    # Tree shape information.
+    d$t1_shape <- lapply(d$t1_nwk, shape_of_str)
+    d$t2_shape <- lapply(d$t2_nwk, shape_of_str)
+    d$t1_colless <- sapply(d$t1_shape, colless)
+    d$t2_colless <- sapply(d$t2_shape, colless)
+
     d
 }
+
+
+
+
