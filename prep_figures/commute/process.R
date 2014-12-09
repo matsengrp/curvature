1282cb2bd30c14f299a7c53aa7d177ac2c5eb316
Erick Matsen <matsen@fhcrc.org>
2014-12-08 18:32:25 -0800

    new file:   curvature-MCT.Rmd

---
+++ prep_figures/commute/process.R
@@ -0,0 +1,20 @@
+library(ape, apTreeshape)
+
+shape_of_str <- function(s) as.treeshape(read.tree(text=s))
+
+
+get_results <- function(path) {
+    d <- read.table(path, stringsAsFactors=FALSE)
+    colnames(d) <- c("t1_idx", "t2_idx", "t1_nwk", "t2_nwk", "dist", "kappa", "MAT", "MCT")
+    d <- subset(d, !is.na(kappa))
+    d <- sample(d)  # Shuffle rows of d.
+    d$info= paste(d$t1_nwk, d$t2_nwk, sep="<br />")
+
+    # Tree shape information.
+    d$t1_shape <- lapply(d$t1_nwk, shape_of_str)
+    d$t2_shape <- lapply(d$t2_nwk, shape_of_str)
+    d$t1_colless <- sapply(d$t1_shape, colless)
+    d$t2_colless <- sapply(d$t2_shape, colless)
+
+    d
+}
