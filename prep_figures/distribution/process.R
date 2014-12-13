bf1f49927067152366a9409574ffe7c20cbdf9c5
Erick Matsen <matsen@fhcrc.org>
2014-12-13 07:57:02 -0800

    Shuffling later.

--- prep_figures/distribution/process.R
+++ prep_figures/distribution/process.R
@@ -6,8 +6,8 @@ shape_of_str <- function(s) as.treeshape(read.tree(text=s))
 get_results <- function(path) {
     d <- read.table(path, stringsAsFactors=FALSE)
     colnames(d) <- c("t1_idx", "t2_idx", "t1_nwk", "t2_nwk", "mult", "dist", "kappa_str")
+    d$row = 1:nrow(d)
     d <- subset(d, kappa_str != "-")
-    d <- sample(d)  # Shuffle rows of d.
     d$kappa <- sapply(d$kappa_str, function(s) eval(parse(text=s)))
     d$info= paste(d$t1_nwk, d$t2_nwk, paste("kappa:", d$kappa_str), sep="<br />")
     d$dist_jitter = d$dist + runif(nrow(d), min = -0.35, max = 0.35)
