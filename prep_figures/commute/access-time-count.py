8e712fc4be6ea8f90a98ceb0a2ca2af458cbca28
Erick Matsen <matsen@fhcrc.org>
2014-12-18 17:15:06 -0800

    Allowing multiple tracefiles.

--- prep_figures/commute/access-time-count.py
+++ prep_figures/commute/access-time-count.py
@@ -2,25 +2,23 @@
 '''
 Suggested use:
 ./commute-time-detail.py --tp \
-    "$(sed -n 60p ../../tangle/rooted-symmetric/tangle5.idx)" <mcmc-trace-file>
+    "$(sed -n 60p ../../tangle/rooted-symmetric/tangle5.idx)" <mcmc-trace-files>
 '''
 
 import argparse
 import collections
-import os
 import re
-import string
 
 parser = argparse.ArgumentParser(description='Find commute times',
                                  prog='gen-tangles.py')
 
-parser.add_argument('tracefile')
+parser.add_argument('tracefiles', nargs='+')
 parser.add_argument(
     '--tp',
     help='A pair of trees of interest. \
     String is trimmed between first open paren and last semicolon.',
     required=True)
-parser.add_argument('-o', help='Out path')
+parser.add_argument('-o', help='Out path', required=True)
 
 args = parser.parse_args()
 tp_str = args.tp
@@ -32,39 +30,30 @@ interesting = [s.rstrip(';') for s in tp_str.split()]
 (s1, s2) = interesting
 print "Searching for: " + str(interesting)
 
-
-def posixify_newick(s):
-    return s.translate(string.maketrans("(),", "CD_"))
-
-trace = args.tracefile
-if args.o is None:
-    outpath = '-'.join([os.path.basename(trace),
-                        posixify_newick(s1),
-                        posixify_newick(s2)])+'.tab'
-else:
-    outpath = args.o
-
-last_seen = {s: None for s in interesting}
 transitions = {}
 for a in interesting:
     transitions[a] = {}
     for b in interesting:
         transitions[a][b] = collections.Counter()
 
-with open(outpath, 'w') as fout:
-    with open(trace, 'r') as fin:
-        for l in fin:
-            (_, idx, _, curr) = l.split()
-            idx = int(idx)
-            if curr in interesting:
-                for prev in interesting:
-                    if last_seen[prev] is not None:
-                        # idx - last_seen[prev] is the number of steps
-                        # between the last time we saw prev and the current
-                        # index.
-                        transitions[prev][curr][idx - last_seen[prev]] += 1
-                last_seen[curr] = idx
-    print 'count\tfrom\tto'
+with open(args.o, 'w') as fout:
+    for trace in args.tracefiles:
+        print trace
+        # Forget if we saw it in another trace file.
+        last_seen = {s: None for s in interesting}
+        with open(trace, 'r') as fin:
+            for l in fin:
+                (_, idx, _, curr) = l.split()
+                idx = int(idx)
+                if curr in interesting:
+                    for prev in interesting:
+                        if last_seen[prev] is not None:
+                            # idx - last_seen[prev] is the number of steps
+                            # between the last time we saw prev and the current
+                            # index.
+                            transitions[prev][curr][idx - last_seen[prev]] += 1
+                    last_seen[curr] = idx
+        print 'count\tfrom\tto'
     for a in interesting:
         for b in interesting:
             for time, freq in transitions[a][b].items():
