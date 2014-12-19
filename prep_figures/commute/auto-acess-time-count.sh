1e26af679e956a2d26dcad9271200ba5ca81f3cf
Erick Matsen <matsen@fhcrc.org>
2014-12-18 16:42:40 -0800

    Less bad names for access distribution stuff.

--- prep_figures/commute/auto-acess-time-count.sh
+++ prep_figures/commute/auto-acess-time-count.sh
@@ -3,7 +3,7 @@ rw_path=/shared/silo_researcher/Matsen_F/MatsenGrp/working/cwhidden/random_walks
 
 for ntangle in 7 14 66 68 69; do
     sedcmd="sed -n ${ntangle}p ../../tangle/rooted-symmetric/tangle5.idx"
-    ./commute-time-detail.py --tp "$($sedcmd)" $rw_path/5-taxa/123_45-1v5c/run1/uniq_trees_T -o runs/run1.${ntangle}.tab
+    ./access-time-count.py --tp "$($sedcmd)" $rw_path/5-taxa/123_45-1v5c/run1/uniq_trees_T -o 5-taxon-access-distn/run1.${ntangle}.tab
 done
 
 #123_456-1v5c.tre
