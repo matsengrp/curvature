72bd04f62407b44d42bd60b75c1fb11298f4c386
Erick Matsen <matsen@fhcrc.org>
2014-12-19 08:19:08 -0800

    More access times.

--- prep_figures/commute/auto-acess-time-count.sh
+++ prep_figures/commute/auto-acess-time-count.sh
@@ -1,13 +1,15 @@
 
 rw_path=/shared/silo_researcher/Matsen_F/MatsenGrp/working/cwhidden/random_walks
 
-# for ntangle in 7 14 66 68 69; do
-#     sedcmd="sed -n ${ntangle}p ../../tangle/rooted-symmetric/tangle5.idx"
-#     ./access-time-count.py --tp "$($sedcmd)" $rw_path/5-taxa/123_45-1v5c/run[1-9]/uniq_trees_T -o 5-taxon-access-distn/${ntangle}.tab
-# done
+#for ntangle in 7 14 66 68 69; do
+#    sedcmd="sed -n ${ntangle}p ../../tangle/rooted-symmetric/tangle5.idx"
+#    ./access-time-count.py --tp "$($sedcmd)" $rw_path/5-taxa/123_45-equal/run[1-9]/uniq_trees_T -o 5-taxon-access-distn/equal/${ntangle}.tab
+#    ./access-time-count.py --tp "$($sedcmd)" $rw_path/5-taxa/123_45-1v5c/run[1-9]/uniq_trees_T -o 5-taxon-access-distn/123_45-1v5c/${ntangle}.tab
+#done
 
 for ntangle in 662 665 795; do
     sedcmd="sed -n ${ntangle}p ../../tangle/rooted-symmetric/tangle6.idx"
-    ./access-time-count.py --tp "$($sedcmd)" $rw_path/6-taxa/123_456-1v5c/run[1-9]/uniq_trees_T -o 6-taxon-access-distn/${ntangle}.tab
+    ./access-time-count.py --tp "$($sedcmd)" $rw_path/6-taxa/123_456-equal/run[1-9]/uniq_trees_T -o 6-taxon-access-distn/equal/${ntangle}.tab
+    #./access-time-count.py --tp "$($sedcmd)" $rw_path/6-taxa/123_456-1v5c/run[1-9]/uniq_trees_T -o 6-taxon-access-distn/123_456-1v5c/${ntangle}.tab
 done
 
