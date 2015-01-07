e2511806620fbfcc9eb1387148c394ba03f1220f
Erick Matsen <matsen@fhcrc.org>
2015-01-07 11:29:50 -0800

    Improving access time scripts.

--- prep_figures/commute/auto-access-time-count.sh
+++ prep_figures/commute/auto-access-time-count.sh
@@ -2,24 +2,27 @@ set -eu
 
 rw_path=/shared/silo_researcher/Matsen_F/MatsenGrp/working/cwhidden/random_walks
 
-# type="*-equal"
-
 # size=5
-# type=123_45-1v5c
+# type=123_45-equal
 
 size=6
-type=123_456-1v5c
+type=123_456-equal
+# type=123_456-1v5c
 
 
-for ntangle in $(awk '{if($8 == $9) print NR}' annotated-rspr/ricci${size}.mat); do
-    sedcmd="sed -n ${ntangle}p ../../tangle/rooted-symmetric/tangle${size}.idx"
+tangle_file=../../tangle/rooted-symmetric/tangle${size}.idx
+
+for ntangle in $(seq $(wc -l $tangle_file | cut -f 1 -d' ')); do # loop over all tangles (yes, self-defeating)
+    sedcmd="sed -n ${ntangle}p $tangle_file"
     outdir=${size}-taxon-access-distn/${type}
     mkdir -p $outdir
     ./access-time-count.py --tp "$($sedcmd)" \
         $rw_path/${size}-taxa/${type}/run[1-9]/uniq_trees_T \
         -o $outdir/${ntangle}.tab &
 
-    while test $(ps -u matsen | grep python | wc -l) -gt 13; do
+    while test $(ps -u matsen | grep python | wc -l) -gt 12; do
         sleep 5;
     done
 done
+
+
