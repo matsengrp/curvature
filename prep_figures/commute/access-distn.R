901435fa4c288408c5e3df5479c6ea8c7b831c73
Erick Matsen <matsen@fredhutch.org>
2015-01-23 16:40:11 -0800

    access-distn: just taking one panel.

--- prep_figures/commute/access-distn.R
+++ prep_figures/commute/access-distn.R
@@ -19,13 +19,14 @@ n_taxa <- 5; favorite_tangles <- all_5_taxon
 data <- "123_45-1v5c/"
 data <- "123_45-equal/"
 
+if(FALSE) {
 all_equal_shape_6_taxon <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 548, 549, 550, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 574, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 662, 665, 691, 692, 693, 694, 695, 696, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708, 709, 710, 711, 712, 713, 714, 715, 716, 717, 718, 719, 720, 721, 722, 723, 724, 725, 726, 727, 728, 729, 776, 777, 778, 779, 780, 781, 782, 794, 795, 796, 797, 798, 799, 800, 801, 802, 803, 804, 805, 806, 807)
 original_6_taxon <-  c(662,665,795)
 all_6_taxon <- 1:807
 n_taxa <- 6; favorite_tangles <- all_6_taxon
-data <- "equal/"
 data <- "123_456-1v5c/"
 data <- "123_456-equal/"
+}
 
 sub_n_taxa <- function(s) sub("NTAXA", as.character(n_taxa), s)
 data_of_num <- function(n) {
@@ -45,11 +46,9 @@ tangles$t1_deg <- sapply(tangles$t1_idx, function(i) degree[i+1]) # tangleX.idx 
 tangles$t2_deg <- sapply(tangles$t2_idx, function(i) degree[i+1]) # tangleX.idx uses 0 indexing.
 tangles$coset <- NULL
 tangles$num <- c(1:nrow(tangles))
-tail(tangles)
 
 d <- do.call(rbind, lapply(favorite_tangles, data_of_num))
 d <- subset(d, t1 != t2)
-head(d)
 d <- merge(d, tangles, by="num")
 d$kappa <- sapply(d$kappa_str, function(s) eval(parse(text=s)))
 
@@ -68,6 +67,13 @@ p <- p + theme(panel.grid.minor =   element_blank(),
                panel.grid.major =   element_line(size=1.))
 p <- p + scale_fill_discrete("distance")
 ggsave(paste0(n_taxa,"-taxon-access-by-degree.svg"), p)
+
+
+
+# ******* Note this filter!
+d <- subset(subset(d, dist==2), t1_deg==24)
+
+
 
 # There are two entries for each (num, time) pair for the two directions,
 # assuming as we do that the trees are distinct.
@@ -91,16 +97,18 @@ make_lims <- function(max_index, column) {
 max_index <- 5
 p <- ggplot(d, aes(x=time, y=mean_dens, group=num, color=kappa, fill=kappa))
 p <- p + geom_line()
-p <- p + scale_x_continuous(limits=c(1,max_index))
+p <- p + scale_x_continuous("access time", limits=c(2,max_index))
 p <- p + scale_y_continuous("density", breaks=c())
 # p <- p + scale_y_continuous("density", limits=make_lims(max_index, "mean_dens"), breaks=c())
-p <- p + facet_grid(dist ~ t1_deg)
-ggsave(paste0(as.character(n_taxa),"-taxon-access-time-kappa-short.svg"), p)
+# p <- p + facet_grid(dist ~ t1_deg)
+p <- p + ggtitle("5 taxa, same shape, d=2, 24 neighbors")
+ggsave(paste0(as.character(n_taxa),"-taxon-access-time-kappa-short.svg"), p, width=9.5)
 
 # Plot showing long time info.
 p <- ggplot(d, aes(x=time, y=mean_dens, group=num, color=kappa)) + geom_line()
-p <- p + scale_x_continuous(limits=c(1,2500)) + scale_y_continuous("density", breaks=c())
-p <- p + facet_grid(dist ~ t1_deg, scales="free")
+p <- p + scale_x_continuous(limits=c(1,500)) + scale_y_continuous("density", breaks=c())
+# p <- p + facet_grid(dist ~ t1_deg, scales="free")
+p <- p + ggtitle("5 taxa, same shape, d=2, 24 neighbors")
 p <- p + theme(axis.text.x=element_text(angle=-90))
-ggsave(paste0(as.character(n_taxa),"-taxon-access-time-kappa-long.png"), p, width=9.5)
+ggsave(paste0(as.character(n_taxa),"-taxon-access-time-kappa-long.svg"), p, width=9.5)
 
