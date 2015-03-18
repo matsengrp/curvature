b97fa1a1d9b5c6219ced8040fbc4c5fe8e2dfb7a
Erick Matsen <matsen@fredhutch.org>
2015-03-18 12:05:21 -0700

    Adding distribution plots too. Ugly!

--- scripts/plot-access.py
+++ scripts/plot-access.py
@@ -26,9 +26,6 @@ parser.add_argument('curvatures', type=str, help='Tangle curvatures.')
 parser.add_argument('--prefix', required=True,
                     type=str, help='Prefix of plot paths for saving.')
 
-parser.add_argument('--which', choices=['hexbin', 'scatter'],
-                    help='What type of plot is desired.')
-
 args = parser.parse_args()
 for p in [args.access, args.tangles, args.degrees, args.curvatures]:
     assert(os.path.exists(p))
@@ -48,25 +45,37 @@ catted['mean_access'] = \
               for (tangle_num, adf) in access.groupby('tangle'))
 catted['tangle'] = catted.index
 
-df_deg_grouped = catted.groupby(['t1_deg','t2_deg', 'dist'])
 t1_degs = catted['t1_deg'].unique()
 t2_degs = catted['t2_deg'].unique()
 
-fig, axs = plt.subplots(
-    len(t1_degs), len(t2_degs),
-    sharex=True,
-    sharey=True,
-    figsize=(4*len(t1_degs), 4*len(t1_degs)),
-    squeeze=False)  # Always return an array of axs, even if 1x1.
+def subplots():
+    return plt.subplots(
+        len(t1_degs), len(t2_degs),
+        sharex=True,
+        sharey=True,
+        figsize=(4*len(t1_degs), 4*len(t1_degs)),
+        squeeze=False)  # Always return an array of axs, even if 1x1.
+e_fig, e_axs = subplots()
+d_fig, d_axs = subplots()
 
-for (t1_deg, t2_deg, dist), group in df_deg_grouped:
+for (t1_deg, t2_deg, dist), group in catted.groupby(['t1_deg','t2_deg', 'dist']):
     i = np.where(t1_degs == t1_deg)[0][0]
     j = np.where(t2_degs == t2_deg)[0][0]
     p = group['mean_access'].plot(
-        ax=axs[i][j], kind='hist', stacked=True, bins=25)
+        ax=e_axs[i][j], kind='hist', stacked=True, bins=25)
     p.grid(b=None)
+
+    norm = mpl.colors.Normalize(group['kappa'].min(), group['kappa'].max())
+    m = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.winter)
+
+    for (idx, _) in group.iterrows():
+        kappa = group['kappa'][idx]
+        sub_d = access[access['tangle'] == idx]
+        d_axs[i][j].plot(sub_d['time'], sub_d['count'], color=m.cmap(kappa), label=kappa)
+        d_axs[i][j].legend()
 
 sns.despine()
 # sns.despine(offset=10, trim=True)
 
-fig.savefig(args.prefix)
+e_fig.savefig(args.prefix+'-expected.svg')
+d_fig.savefig(args.prefix+'-distribution.svg')
