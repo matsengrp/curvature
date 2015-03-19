b341ac08a64cf02b1dbe4c1a6f86fb00074414e9
Erick Matsen <matsen@fredhutch.org>
2015-03-18 17:00:55 -0700

    More deuglifying.

--- scripts/plot-access.py
+++ scripts/plot-access.py
@@ -25,6 +25,10 @@ parser.add_argument('degrees', type=str, help='Degrees file.')
 parser.add_argument('curvatures', type=str, help='Tangle curvatures.')
 parser.add_argument('--prefix', required=True,
                     type=str, help='Prefix of plot paths for saving.')
+parser.add_argument('--maxtime',
+                    type=int, help='Maximum time for distribution plotting.')
+parser.add_argument('--dist',
+                    type=int, help='Restrict to this distance.')
 
 args = parser.parse_args()
 for p in [args.access, args.tangles, args.degrees, args.curvatures]:
@@ -64,12 +68,18 @@ cmaps = map(mpl.cm.get_cmap, ['spring', 'summer', 'autumn', 'winter', 'cool',
                               'copper', 'gray', 'hot', 'pink'])
 
 for (t1_deg, t2_deg, dist), group in catted.groupby(['t1_deg','t2_deg', 'dist']):
+    if args.dist and dist != args.dist:
+        continue
+    title_str = 'deg1 = {},  deg2 = {}'.format(t1_deg, t2_deg)
+
     i = np.where(t1_degs == t1_deg)[0][0]
     j = np.where(t2_degs == t2_deg)[0][0]
+    e_axs[i][j].set_title(title_str, weight=500)
     p = group['mean_access'].plot(
         ax=e_axs[i][j], kind='hist', stacked=True, bins=25)
     p.grid(b=None)
 
+    d_axs[i][j].set_title(title_str, weight=500)
     norm = mpl.colors.Normalize(group['kappa'].min(), group['kappa'].max())
     m = mpl.cm.ScalarMappable(norm=norm, cmap=cmaps[dist])
 
@@ -78,12 +88,14 @@ for (t1_deg, t2_deg, dist), group in catted.groupby(['t1_deg','t2_deg', 'dist'])
         if np.isnan(kappa):
             continue
         sub_d = access[access['tangle'] == idx]
+        if args.maxtime:
+            sub_d = sub_d[sub_d['time'] < args.maxtime]
         d_axs[i][j].plot(sub_d['time'], sub_d['count'],
                          color=m.cmap(kappa), label=kappa)
-        d_axs[i][j].legend()
+        # d_axs[i][j].legend()
 
-sns.despine()
-# sns.despine(offset=10, trim=True)
+sns.despine(offset=4, trim=True)
+plt.tight_layout()
 
 e_fig.savefig(args.prefix+'-expected.svg')
 d_fig.savefig(args.prefix+'-distribution.svg')
