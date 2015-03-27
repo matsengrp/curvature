ba536ea248d0d5ae1238497e7adf45c5125c3fee
Erick Matsen <matsen@fredhutch.org>
2015-03-27 15:27:24 -0600

    Better plot names.

--- prefigs/get-figs.sh
+++ prefigs/get-figs.sh
@@ -1,5 +1,5 @@
 for graph in nni rspr; do
     for i in ../analysis/6-walks-50K/$graph/upmh/scatter*svg; do
-        cp $i $graph-$(basename $i)
+        cp $i $graph-upmh-$(basename $i)
     done
 done
